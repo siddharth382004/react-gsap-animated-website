08bb4d320a8585ff9580b14d77239685a2dc1e47
siddharth382004 <168186431+siddharth382004@users.noreply.github.com>
2025-11-04 00:30:46 +0530

    Moved video file to src/assets and fixed Video.jsx import

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import videoFile from "../../assets/video.mp4";
 
 const Video = () => {
   return (
-    <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+    <div className="h-full w-full">
+      <video
+        className="h-full w-full object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src={videoFile} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
